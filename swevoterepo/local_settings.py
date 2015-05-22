dfa5778fabe56df8d4684672a5b0159f7fd9c71b
Lina Huang <linahuang94@gmail.com>
2015-05-22 17:15:19 -0400

    initial

---
+++ swevoterepo/local_settings.py
@@ -0,0 +1,7 @@
+#Preproduction
+
+ #Static files (CSS, JavaScript, Images)
+ #https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join('static'), )
+# SECURITY WARNING: don't run with debug turned on in production!
